1446bbdf42301ba9eb9d1fb5e4e7b67d51face98
krishna <154574691+KrishnaDileepG@users.noreply.github.com>
2025-03-19 15:53:09 +0530

    broadcast completed

--- index.js
+++ index.js
@@ -10,28 +10,41 @@ for await (const connection of listener) {
 }
 
 async function handleConnection(connection, clientId) {
-  while (true) {
-    const buf = new Uint8Array(1024);
-    const bytesCount = await connection.read(buf);
-    const decoder = new TextDecoder();
-    const encoder = new TextEncoder();
-    const msg = decoder.decode(buf.slice(0, bytesCount));
-    console.log(` ${clientId}) ${msg}`);
-    
-    if (msg.trim() === "exit") {
-      await connection.write(encoder.encode("Bye\n"));
-      connection.close();
-      return;
-    }
-    const broadCast = prompt("Server broadcast: ");
-    
-    for (const c of clients) {
-      await c.conn.write
-        (encoder.encode(`Server saying: ${broadCast}\n`));
-    }
+  try {
+    while (true) {
+      const buf = new Uint8Array(1024);
+      const bytesCount = await connection.read(buf);
+      const decoder = new TextDecoder();
+      const encoder = new TextEncoder();
+      const msg = decoder.decode(buf.slice(0, bytesCount));
+      console.log(` ${clientId}) ${msg}`);
 
-    // const reply = prompt("Enter the reply");
+      if (msg.trim() === "exit") {
+        await connection.write(encoder.encode("Bye\n"));
+        connection.close();
+        removeClient(connection);
+        return;
+      }
+      const broadCast = prompt("Server broadcast: ");
 
-    // connection.write(encoder.encode(`Server > Client ${clientId}: ${reply}\n`));
+      for (const c of clients) {
+        await c.conn.write(encoder.encode(`Server saying: ${broadCast}\n`));
+      }
+
+      const reply = prompt("Enter the reply");
+
+      connection.write(
+        encoder.encode(`Server > Client ${clientId}: ${reply}\n`)
+      );
+    }
+  } catch {
+    return;
+  }
+}
+
+function removeClient(conn) {
+  const index = clients.findIndex((c) => c.conn === conn);
+  if (index !== -1) {
+    clients.splice(index, 1);
   }
 }
